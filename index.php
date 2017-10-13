8cef51f7c933b3fcc3ddc316b4e0cdcd046116e5
Ing. Eduardo San Miguel Domínguez García <atnasetreum@gmail.com>
2017-10-13 14:36:28 -0500

    Update index.php

--- index.php
+++ index.php
@@ -17,10 +17,6 @@
         </div>
     </div>
 
-
-
-
-
     <script src="src/js/jquery-3.2.1.min.js" ></script>
     <script src="src/js/notifica.js" ></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
